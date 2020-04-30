a2a9feee63112f7d48d2caaced48ef04e1e553e6
Sergiu Ionescu <sergiu.ionescu@gmail.com>
2020-04-30 21:54:35 +0300

    Use ESPAsyncWebServer

--- esp32.ino
+++ esp32.ino
@@ -1,5 +1,7 @@
 #include "WiFi.h"
-#include "WebServer.h"
+#include "ESPAsyncWebServer.h"
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
 
 #ifndef max
 #define max(a,b) (((a) > (b)) ? (a) : (b))
@@ -14,7 +16,7 @@ const int echoPinFR = 14;
 const int maxDistance = 200;
 const int maxPower = 256;
 const int minPower = -256;
-const float distanceToPowerMultiplier = 2;
+const float distanceToPowerMultiplier = 0.5;
 
 const char* ssid     = "ESP32";
 const char* password = "987654321";
@@ -22,6 +24,9 @@ const char* password = "987654321";
 long duration;
 int distanceFL;
 int distanceFR;
+
+int motorRightReference;
+int motorLeftReference;
 
 struct Channel {
     int en1;
@@ -45,8 +50,7 @@ IPAddress local_ip(192,168,1,1);
 IPAddress gateway(192,168,1,1);
 IPAddress subnet(255,255,255,0);
 
-WebServer server(80);
-String header;
+AsyncWebServer server(80);
 
 void setup() {
   Serial.begin(115200);
@@ -85,7 +89,7 @@ void setup() {
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
   
-  server.on("/", handleIndex);
+  server.on("/", HTTP_GET, handleIndex);
   server.on("/data", handleData);
   
   server.begin();
@@ -94,50 +98,55 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
   updateDistance();  
   updatePWM();
 }
 
 
-void handleIndex() {
-  server.send(200, "text/html", "<html>  <head>    <style>      body {        font-size: 2em;      }      div.container {        max-width: 800px;        max-height: 600px;        width: 100%;        height: 100%;        background-color: lightgray;        position: absolute;        top: 0;        bottom: 0;        left: 0;        right: 0;        margin: auto;      }      div.progress {        position: absolute;        margin: auto;        width: 45%;        height: 2%;        background-color: blueviolet;      }      div.control {        position: absolute;        margin: auto;        width: 80%;        height: 70%;        top: 0;        bottom: 0;        left: 0;        right: 0;      }      div.left {        background-color: gray;              position: absolute;        top: 0;        bottom: 0;        left: 0;        margin: auto;      }      div.right {        background-color: gray;              position: absolute;        top: 0;        bottom: 0;        right: 0;        margin: auto;      }      .throttle {        height: 80%;        width: 100px;        padding: 0 15px;        -webkit-appearance: slider-vertical;      }      .vcenter {        position: absolute;        left: 0;        right: 0;        margin: auto;      }      label {        position: absolute;        left: 0;        right: 0;        display: block;        margin-bottom: 10px;      }      div.slider {        width: 100px;      }      div.actions {        position: absolute;        top: 0;        bottom: 0;        left: 0;        right: 0;        margin: auto;        width:300px;      }      input {        font-size: 30px;      }          </style>  </head>  <body>    <div class='container'>      <div class='progress' id='distanceFL' style='left:0;top:0'></div>      <div class='progress' id='distanceFR' style='right:0;top:0'></div>      <div class='control'>        <div class='left slider'>            <label id='left_label' for='left'>0</label>            <input type='range' id='left' class='throttle vcenter' min='-255' max='255'/>        </div>        <div class='actions'>          <input id='locked' type='button' value='Unlock'/>        </div>        <div class='right slider'>            <label id='right_label' for='right'>0</label>            <input type='range' id='right' class='throttle vcenter' min='-255' max='255'/>        </div>      </div>      <div class='progress' id='distanceBL' style='left:0;bottom:0'></div>      <div class='progress' id='distanceBR' style='right:0;bottom:0'></div>    </div>    <script>      function updateDistance(id, data) {        var distance = Math.min(data[id], 200);        var newWidth = parseInt(distance/5);        document.getElementById(id).style.width = newWidth + '%';        document.getElementById(id).innerHTML = distance + 'cm';      }      function getProgress() {        var xhttp = new XMLHttpRequest();        var left = document.getElementById('left').value;        var right = document.getElementById('right').value;        xhttp.onreadystatechange = function() {          if (this.readyState == 4 && this.status == 200) {            var response = JSON.parse(this.responseText.trim());            updateDistance('distanceFL', response['data']);            updateDistance('distanceFR', response['data']);            updateDistance('distanceBL', response['data']);            updateDistance('distanceBR', response['data']);                        setTimeout(getProgress, 500);          }        };        xhttp.onerror = function(e){          setTimeout(getProgress, 250);        };        xhttp.open('GET', '/data?right=' + right + '&left=' + left, true);        xhttp.send();      }      getProgress();      var locked = true;      document.getElementById('locked').addEventListener(        'click', function () {          if(locked) {            document.getElementById('locked').value = 'Lock';            locked = false;          } else {            document.getElementById('locked').value = 'Unlock';            locked = true;          }          document.getElementById('right').value = 0;          document.getElementById('right_label').innerText = 0;          document.getElementById('left').value = 0;          document.getElementById('left_label').innerText = 0;        }      );      document.getElementById('left').addEventListener(        'input', function () {          document.getElementById('left_label').innerText = document.getElementById('left').value;          if(locked) {            document.getElementById('right').value = document.getElementById('left').value;            document.getElementById('right_label').innerText = document.getElementById('left').value;          }        }      );      document.getElementById('right').addEventListener(        'input', function () {          document.getElementById('right_label').innerText = document.getElementById('right').value;          if(locked) {            document.getElementById('left').value = document.getElementById('right').value;            document.getElementById('left_label').innerText = document.getElementById('right').value;          }        }      );    </script>  </body></html>\n");
+void handleIndex(AsyncWebServerRequest *request) {
+  request->send(200, "text/html", "<html>  <head>    <style>      body {        font-size: 2em;      }      div.container {        max-width: 800px;        max-height: 600px;        width: 100%;        height: 100%;        background-color: lightgray;        position: absolute;        top: 0;        bottom: 0;        left: 0;        right: 0;        margin: auto;      }      div.progress {        position: absolute;        margin: auto;        width: 45%;        height: 2%;        background-color: blueviolet;      }      div.control {        position: absolute;        margin: auto;        width: 80%;        height: 70%;        top: 0;        bottom: 0;        left: 0;        right: 0;      }      div.left {        background-color: gray;              position: absolute;        top: 0;        bottom: 0;        left: 0;        margin: auto;      }      div.right {        background-color: gray;              position: absolute;        top: 0;        bottom: 0;        right: 0;        margin: auto;      }      .throttle {        height: 80%;        width: 100px;        padding: 0 15px;        -webkit-appearance: slider-vertical;      }      .vcenter {        position: absolute;        left: 0;        right: 0;        margin: auto;      }      label {        position: absolute;        left: 0;        right: 0;        display: block;        margin-bottom: 10px;      }      div.slider {        width: 100px;      }      div.actions {        position: absolute;        top: 0;        bottom: 0;        left: 0;        right: 0;        margin: auto;        width:300px;      }      input {        font-size: 30px;      }          </style>  </head>  <body>    <div class='container'>      <div class='progress' id='distanceFL' style='left:0;top:0'></div>      <div class='progress' id='distanceFR' style='right:0;top:0'></div>      <div class='control'>        <div class='left slider'>            <label id='left_label' for='left'>0</label>            <input type='range' id='left' class='throttle vcenter' min='-255' max='255'/>        </div>        <div class='actions'>          <input id='locked' type='button' value='Unlock'/>        </div>        <div class='right slider'>            <label id='right_label' for='right'>0</label>            <input type='range' id='right' class='throttle vcenter' min='-255' max='255'/>        </div>      </div>      <div class='progress' id='distanceBL' style='left:0;bottom:0'></div>      <div class='progress' id='distanceBR' style='right:0;bottom:0'></div>    </div>    <script>      function updateDistance(id, data) {        var distance = Math.min(data[id], 200);        var newWidth = parseInt(distance/5);        document.getElementById(id).style.width = newWidth + '%';        document.getElementById(id).innerHTML = distance + 'cm';      }      function getProgress() {        var xhttp = new XMLHttpRequest();        var left = document.getElementById('left').value;        var right = document.getElementById('right').value;        xhttp.onreadystatechange = function() {          if (this.readyState == 4 && this.status == 200) {            var response = JSON.parse(this.responseText.trim());            console.log(this.responseText);            updateDistance('distanceFL', response['data']);            updateDistance('distanceFR', response['data']);                        setTimeout(getProgress, 250);          }        };        xhttp.onerror = function(e){          setTimeout(getProgress, 50);        };        xhttp.open('GET', '/data?right=' + right + '&left=' + left, true);        xhttp.send();      }      getProgress();      var locked = true;      document.getElementById('locked').addEventListener(        'click', function () {          if(locked) {            document.getElementById('locked').value = 'Lock';            locked = false;          } else {            document.getElementById('locked').value = 'Unlock';            locked = true;          }          document.getElementById('right').value = 0;          document.getElementById('right_label').innerText = 0;          document.getElementById('left').value = 0;          document.getElementById('left_label').innerText = 0;        }      );      document.getElementById('left').addEventListener(        'input', function () {          document.getElementById('left_label').innerText = document.getElementById('left').value;          if(locked) {            document.getElementById('right').value = document.getElementById('left').value;            document.getElementById('right_label').innerText = document.getElementById('left').value;          }        }      );      document.getElementById('right').addEventListener(        'input', function () {          document.getElementById('right_label').innerText = document.getElementById('right').value;          if(locked) {            document.getElementById('left').value = document.getElementById('right').value;            document.getElementById('left_label').innerText = document.getElementById('right').value;          }        }      );    </script>  </body></html>\n");
 }
 
-void handleData() {
+void handleData(AsyncWebServerRequest *request) {
   String message;
-  for (int i = 0; i < server.args(); i++) {
-    message += "Arg n" + (String)i + " –> ";
-    message += server.argName(i) + ": ";
-    message += server.arg(i) + "\n";
+  String responseText;
+  int count = request->params();
+  for (int i = 0; i < count; i++) {
+    AsyncWebParameter* param = request->getParam(i);
+    message += "Param n" + (String)i + " –> ";
+    message += param->name() + ": ";
+    message += param->value() + "\n";
 
-    if(server.argName(i) == "right") {
-      motorRight.value = server.arg(i).toInt();
+    if(param->name() == "right") {
+      motorRightReference = param->value().toInt();
     }
-    if(server.argName(i) == "left") {
-      motorLeft.value = server.arg(i).toInt();
+    if(param->name() == "left") {
+      motorLeftReference = param->value().toInt();
     }
   } 
   Serial.println(message);
 
+  AsyncResponseStream *response = request->beginResponseStream("application/json");
   
-  String response = "";
-  response += "{\"data\":{\"motors\":";
-  response += "{\"left\":{\"value\":";
-  response += motorLeft.value;
-  response += "},\"right\":{\"value\":";
-  response += motorRight.value;
-  response += "}},";
-  response += "\"distanceFL\":";
-  response += distanceFL;
-  response += ",\"distanceFR\":";
-  response += distanceFR;
-  response += ",\"distanceBL\":";
-  response += 0;
-  response += ",\"distanceBR\":";
-  response += 0;
-  response += "}}\n";
-  server.send(200, "text/html", response);
+  const size_t capacity = 3*JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(5);
+  DynamicJsonDocument doc(capacity);
+
+  JsonObject data = doc.createNestedObject("data");
+  
+  JsonObject data_motors = data.createNestedObject("motors");
+  JsonObject data_motors_left = data_motors.createNestedObject("left");
+  data_motors_left["value"] = motorLeft.value;
+  JsonObject data_motors_right = data_motors.createNestedObject("right");
+  data_motors_right["value"] = motorRight.value;
+  data["distanceFL"] = distanceFL;
+  data["distanceFR"] = distanceFR;
+  data["distanceBL"] = 0;
+  data["distanceBR"] = 0;
+
+  serializeJson(doc, responseText);
+  response->print(responseText);
+  
+  request->send(response);
 }
 
 
@@ -161,6 +170,8 @@ void updateDistance() {
 }
 
 void updatePWM(){
+    motorRight.value = motorRightReference;
+    motorLeft.value = motorLeftReference;
     if(motorRight.value > 0 && distanceFL < 100) {
       motorRight.value = max(motorRight.value - (maxDistance - distanceFL) * distanceToPowerMultiplier, minPower);
     }
@@ -169,7 +180,7 @@ void updatePWM(){
     }
     
     //Channel 1
-    if(motorRight.value > 0){
+    if(motorRight.value < 0){
         digitalWrite(motorRight.en1, LOW);
         digitalWrite(motorRight.en2, HIGH);
     }else{
@@ -179,7 +190,7 @@ void updatePWM(){
     ledcWrite(pwmChannelLeft, abs(motorRight.value));
 
     //Channel 2
-    if(motorLeft.value > 0){
+    if(motorLeft.value < 0){
         digitalWrite(motorLeft.en1, LOW);
         digitalWrite(motorLeft.en2, HIGH);
     }else{
